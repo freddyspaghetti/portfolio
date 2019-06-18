40544d654c276a698e020b589258484819363605
freddyspaghetti <frederik.bjorgensen@gmail.com>
2019-06-19 01:20:59 +0200

    menu virker YES SIR!

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    document.querySelector("#mere").addEventListener("click", readMore)
+    document.querySelector("#mere").addEventListener("click", readMore);
 }
 
 function readMore() {
     console.log("readMore:");
     document.querySelector("#box").classList.toggle("stor");
     document.querySelector("#box").classList.toggle("lille");
+}
+
+window.addEventListener("load", menuVirker);
+
+function menuVirker() {
+    console.log("menu virker");
+    document.querySelector("#menuknapMobil").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menuMobil").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menuMobil").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.getElementById("menuknapMobil").src = "svg/spaghetti-menu.svg";
+    } else {
+        document.getElementById("menuknapMobil").src = "svg/spaghetti-no.svg";
+    }
 }
